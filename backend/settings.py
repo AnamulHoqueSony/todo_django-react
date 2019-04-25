d50c7e39b60be418e141bd52275a5ab88919dd24
Anamul_ru <roni.twenty@gmail.com>
2019-04-25 16:32:24 +0600

    todo backenc

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -114,7 +117,12 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITLIST = (
+    'localhost:3000/'
+)
